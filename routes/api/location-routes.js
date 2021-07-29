8950795c54d15bc47ab7bf8bb7f64aedcc250507
TheHebi <nturcotte8@live.com>
2021-07-29 11:52:52 -0700

    trip add and delete working

--- routes/api/location-routes.js
+++ routes/api/location-routes.js
@@ -3,25 +3,51 @@ const { builtinModules } = require("module");
 const { Trip, Traveler, Location } = require("../../models");
 
 router.get("/", async (req, res) => {
-  
+  try{
+    const location = await Location.findAll({
+      attributes: {exclude: [`createdAt`,`updatedAt`]}
+    })
+    res.status(200).json(location)
+  }catch (err){
+    console.log(err)
+    res.status(500).json(err)
+  }
 });
 
 router.get("/:id", async (req, res) => {
-  try {
-    res.status(200).json(tag);
-  } catch (err) {}
+  try{
+    const traveler = await Location.findOne({
+      where: {id:req.params.id},
+      include: {model:Trip, attributes: {exclude: [`createdAt`,`updatedAt`]}},
+      attributes: {exclude: [`createdAt`,`updatedAt`]},
+    })
+    res.status(200).json(traveler)
+  }catch (err){
+    console.log(err)
+    res.status(500).json(err)
+  }
 });
 
 router.post("/", async (req, res) => {
-  try {
-    res.status(200).json(tag);
-  } catch (err) {}
+  try{
+    const newLocation = await Location.create(req.body);
+    res.status(200).json(newLocation)
+  }catch(err){
+    console.log(err)
+    res.status(400).json(err)
+  }
 });
 
 router.delete("/:id", async (req, res) => {
-  try {
-    res.status(200).json(tag);
-  } catch (err) {}
+  try{
+    const delLocation = await Location.destroy({
+      where:{id:req.params.id}
+    });
+    res.status(200).json(delLocation)
+  }catch(err){
+    console.log(err)
+    res.status(400).json(err)
+  }
 });
 
 module.exports = router;
--- routes/api/trip-routes.js
+++ routes/api/trip-routes.js
@@ -2,15 +2,25 @@ const router = require("express").Router();
 const { Trip, Traveler, Location } = require("../../models");
 
 router.post("/", async (req, res) => {
-  try {
-    res.status(200).json(tag);
-  } catch (err) {}
+  try{
+    const newTrip = await Trip.create(req.body);
+    res.status(200).json(newTrip)
+  }catch(err){
+    console.log(err)
+    res.status(400).json(err)
+  }
 });
 
 router.delete("/:id", async (req, res) => {
-  try {
-    res.status(200).json(tag);
-  } catch (err) {}
+  try{
+    const delTrip = await Trip.destroy({
+      where:{id:req.params.id}
+    });
+    res.status(200).json(delTrip)
+  }catch(err){
+    console.log(err)
+    res.status(400).json(err)
+  }
 });
 
 module.exports = router;
